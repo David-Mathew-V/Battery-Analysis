a2e5370252e5b4113e23b81fdec8ec72d422963b
david-mathew-v <david.vmathewdavid@gmail.com>
2026-07-17 12:09:14 +0530

    setting configurations

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 # config.py
 
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "david",
-    "password": "root123",
-    "database": "battery",
-    "port": 3306
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "port": int(os.getenv("DB_PORT")),
 }
